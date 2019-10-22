161b46f19e1a5831d7962af5cb50f1ceea9d9057
YmraNaissur <daily.spb@yandex.ru>
2019-10-22 21:14:24 +0300

    Initial version of resume.

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src='https://1.bp.blogspot.com/-exunHSPqM9Y/V90rpBO-nxI/AAAAAAAAQYg/xVUKuvwnWYUMDPMCADnd7kLezorZxeb6QCLcB/s1600/lab%2Brat%2Bface.png'
+                alt="Logo" />
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+        <div>
+          <img src="https://pbs.twimg.com/media/DuzjZjjVAAAaTOK.jpg" alt="Background"
+            width="100%" height="500px" />
+        </div>
+        <div>
+          <img src="https://www.4d83.net/wp-content/uploads/sites/2704/2017/02/VECTO_4D_PICTO_Rats_Souris.png"
+            alt="Rat logo" />
+        </div>
+        <div>
+          My posts
+          <div>
+            New post
+          </div>
+          <div>
+            <div>
+              Post 1
+          </div>
+            <div>
+              Post 2
+          </div>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Profile;
