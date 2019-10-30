2a540283402a51f9a05a414eaae0c98e78e48e65
YmraNaissur <daily.spb@yandex.ru>
2019-10-30 22:33:21 +0300

    Make changes according to lesson 14 (CSS modules).

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import classes from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className="header">
+        <header className={classes.header}>
             <img src='https://1.bp.blogspot.com/-exunHSPqM9Y/V90rpBO-nxI/AAAAAAAAQYg/xVUKuvwnWYUMDPMCADnd7kLezorZxeb6QCLcB/s1600/lab%2Brat%2Bface.png'
                 alt="Logo" />
         </header>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,32 +1,33 @@
 import React from 'react';
+import classes from './Profile.module.css';
 
 const Profile = () => {
-    return (
-        <div className="content">
-        <div>
-          <img src="https://pbs.twimg.com/media/DuzjZjjVAAAaTOK.jpg" alt="Background"
-            width="100%" height="500px" />
-        </div>
-        <div>
-          <img src="https://www.4d83.net/wp-content/uploads/sites/2704/2017/02/VECTO_4D_PICTO_Rats_Souris.png"
-            alt="Rat logo" />
-        </div>
-        <div>
-          My posts
+  return (
+    <div className={classes.content}>
+      <div>
+        <img src="https://pbs.twimg.com/media/DuzjZjjVAAAaTOK.jpg" alt="Background"
+          width="100%" height="250px" />
+      </div>
+      <div>
+        <img src="https://www.4d83.net/wp-content/uploads/sites/2704/2017/02/VECTO_4D_PICTO_Rats_Souris.png"
+          alt="Rat logo" />
+      </div>
+      <div>
+        My posts
           <div>
-            New post
+          New post
           </div>
-          <div>
-            <div>
-              Post 1
+        <div className={classes.posts}>
+          <div className={classes.item}>
+            Post 1
           </div>
-            <div>
-              Post 2
-          </div>
+          <div className={classes.item}>
+            Post 2
           </div>
         </div>
       </div>
-    )
+    </div>
+  )
 }
 
 export default Profile;
