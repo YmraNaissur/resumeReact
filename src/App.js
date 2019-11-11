7391d4d2655ddaf2c25cc2e07e1bb0beea61c85c
mkaravae <maxim.karavaev@t-systems.com>
2019-11-11 17:59:49 +0300

    Lift up BrowserRouter. Handle "Add post" button click according to lesson 32.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
@@ -11,23 +11,21 @@ import Settings from './components/Settings/Settings';
 
 const App = (props) => {
     return (
-        <BrowserRouter>
-            <div className="app-wrapper">
-                <Header />
-                <NavBar state={props.state.sidebar} />
-                <div className="app-wrapper-content">
-                    <Route path="/profile">
-                        <Profile state={props.state.profile} />
-                    </Route>
-                    <Route path="/dialogs">
-                        <Dialogs state={props.state.messages} />
-                    </Route>
-                    <Route path="/news" component={News} />
-                    <Route path="/music" component={Music} />
-                    <Route path="/settings" component={Settings} />
-                </div>
+        <div className="app-wrapper">
+            <Header />
+            <NavBar state={props.state.sidebar} />
+            <div className="app-wrapper-content">
+                <Route path="/profile">
+                    <Profile state={props.state.profile} />
+                </Route>
+                <Route path="/dialogs">
+                    <Dialogs state={props.state.messages} />
+                </Route>
+                <Route path="/news" component={News} />
+                <Route path="/music" component={Music} />
+                <Route path="/settings" component={Settings} />
             </div>
-        </BrowserRouter>
+        </div>
     );
 }
 
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -8,14 +8,24 @@ const MyPosts = (props) => {
         return <Post message={p.message} avatar={p.avatar} likeCount={p.likeCount} />
     });
 
+    let newPostElement = React.createRef();
+
+    let addPost = () => {
+        let text = newPostElement.current.value;
+        alert("text: " + text);
+        
+        // never do this!!!
+        // alert(document.getElementById("new-post").value);
+    }
+
     return (
         <div className={classes.postsBlock}>
             <h3>My posts</h3>
             <div>
-                <textarea></textarea>
+                <textarea ref={newPostElement}></textarea>
             </div>
             <div>
-                <button>Add post</button>
+                <button onClick={addPost}>Add post</button>
             </div>
             <div className={classes.posts}>
                 { postsComponents }
